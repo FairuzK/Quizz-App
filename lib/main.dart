5ee87060d4c953bdc3d903ca29e56e0e911b96f8
Fairuz Kaweesa <kaweesafairuz@gmail.com>
2023-05-01 11:28:57 -0700

    Final Commit to the Quizz App...

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizzerPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizzerPage extends StatefulWidget {
+  const QuizzerPage({super.key});
+
+  @override
+  State<QuizzerPage> createState() => _QuizzerPageState();
+}
+
+class _QuizzerPageState extends State<QuizzerPage> {
+  List<Icon> scoreKeeper = [];
+  void CheckAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+      if (userPickedAnswer == correctAnswer) {
+        scoreKeeper.add(const Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(const Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+
+      quizBrain.nextQuestion();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.green),
+              onPressed: () {
+                setState(() {
+                  bool correctanswer = quizBrain.getQuestionAnswer();
+                  CheckAnswer(true);
+                });
+              },
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              onPressed: () {
+                bool correctanswer = quizBrain.getQuestionAnswer();
+                CheckAnswer(false);
+              },
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/questions.dart
@@ -0,0 +1,8 @@
+class Question {
+  String questionText='';
+  bool questionAnswer=true;
+  Question(String a, bool b, ) {
+    questionText = a;
+    questionAnswer=b;
+  }
+}
